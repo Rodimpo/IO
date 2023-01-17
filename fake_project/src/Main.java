7ecd0eed4e5d640510ce5a39f1ade1e92eeb63c0
Rodimpo <patrykkkollek@wp.pl>
2023-01-17 18:37:57 +0100

    S&S

---
+++ fake_project/src/Main.java
@@ -0,0 +1,25 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class Main
+{
+    JFrame frame;
+    Main()
+    {
+        frame = new JFrame();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(1000,1000);
+
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        Main ebe = new Main();
+    }
+}
